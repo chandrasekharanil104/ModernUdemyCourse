48d11c79991645c292b271ddc504d2640b7d5602
chandrasekharanil <chandrasekharanil104@gmail.com>
2022-12-03 11:26:50 -0600

    PROPS EXAMPLE

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el); // controls the contents of the container node you pass in. Any existing DOM elements inside are replaced when render is called.
 
 const App = function () {
+	const abcobject = { name: "Monkey D Luffy", number: 23 };
 	return (
 		<div>
-			<h1> Hi There! </h1>
+			<input objectdisplay={abcobject} />{" "}
+			{/* We can display object inside the input as props. */}
+			{/* <h1>{abcobject}</h1> */}{" "}
+			{/* Displaying object like this throws an error but we can display the object as props. NOTE:  Object cannot be displayed inside the JSX tags.!!!! */}
+			<input objectdisplay={abcobject} />
 		</div>
 	);
 };
