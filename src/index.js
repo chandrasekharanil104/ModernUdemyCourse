a300770b9b0e4dfa8a4e673dd7621a708864fc84
chandrasekharanil <chandrasekharanil104@gmail.com>
2022-12-04 16:06:17 -0600

    ProfileCard First Commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el); // controls the contents of the container node you pass in. Any existing DOM elements inside are replaced when render is called.
-
-const App = function () {
-	const abcobject = { name: "Monkey D Luffy", number: 23 };
-	return (
-		<div>
-			<input objectdisplay={abcobject} />
-			{/* We can display object inside the input as props. */}
-			{/* <h1>{abcobject}</h1> */}{" "}
-			{/* Displaying object like this throws an error but we can display the object as props. NOTE:  Object cannot be displayed inside the JSX tags.!!!! */}
-			{/* <input objectdisplay={abcobject} /> */}
-			<input type="number" />
-		</div>
-	);
-};
 
 root.render(<App />);
