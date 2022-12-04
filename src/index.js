6bd4efe96946e0b51904799130cf5b501acf5c6b
chandrasekharanil <chandrasekharanil104@gmail.com>
2022-12-04 13:56:59 -0600

    Delete content from index.js and create App.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ const App = function () {
 	const abcobject = { name: "Monkey D Luffy", number: 23 };
 	return (
 		<div>
-			<input objectdisplay={abcobject} />{" "}
+			<input objectdisplay={abcobject} />
 			{/* We can display object inside the input as props. */}
 			{/* <h1>{abcobject}</h1> */}{" "}
 			{/* Displaying object like this throws an error but we can display the object as props. NOTE:  Object cannot be displayed inside the JSX tags.!!!! */}
-			<input objectdisplay={abcobject} />
+			{/* <input objectdisplay={abcobject} /> */}
+			<input type="number" />
 		</div>
 	);
 };
