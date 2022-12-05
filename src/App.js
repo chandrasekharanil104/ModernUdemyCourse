9e4f67cfeeac572dfa2945b3ba3e5d19359949e4
chandrasekharanil <chandrasekharanil104@gmail.com>
2022-12-05 15:01:26 -0600

    Final Commit of Profile Card Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import "bulma/css/bulma.css";
+import React from "react";
 import ProfileCard from "./ProfileCard"; // importing the profile card component.
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 const App = function () {
 	return (
 		<div>
-			<div>Personal Digital Assitant </div>
+			<section className="hero is-primary">
+				<div className="hero-body"></div>
+				<p className="title">Personal Digital Assitants</p>
+			</section>
 			{/* Reusability of the component */}
 			{/* Below the attributes are declared for the components which are called props. Props are passed from parent to child as an object. I.E from APP.js to ProfileCard.js */}
-			<ProfileCard title="siri" handle="@Siri10" />{" "}
-			<ProfileCard title="cortana" handle="Cortana07" />
-			<ProfileCard title="alexa" handle="Alexa23" />
+			<div className="container">
+				<section className="section">
+					<div className="columns">
+						<div className="column is-4">
+							<ProfileCard title="siri" handle="@Siri10" image={AlexaImage} />
+						</div>
+						<div className="column is-4">
+							<ProfileCard
+								title="cortana"
+								handle="Cortana07"
+								image={CortanaImage}
+							/>
+						</div>
+						<div className="column is-4">
+							<ProfileCard title="alexa" handle="Alexa23" image={SiriImage} />
+						</div>
+					</div>
+				</section>
+			</div>
 		</div>
 	);
 };
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,16 +2,21 @@ import React from "react";
 
 // Creating a Separate file for Profile Component.
 
-const ProfileCard = ({ title, handle }) => {
-	console.log("TITLE & HANDLE", title, handle);
-	{
-		/* Props are consumed here which are passed down from App.js*/
-	}
+const ProfileCard = ({ title, handle, image }) => {
+	/* Props are consumed here which are passed down from App.js*/
 	// console.log("PROPS ARE: ", props);    // Here you can see the props which are passed from App.js. Props is an object with the attributes specified on the component. NOTE: this can only be seen if props are specified as function arguments. Not when they are destructured.
 	return (
-		<div>
-			<div>
-				The title is {title} and the social media handle is {handle}.
+		<div className="card">
+			<div className="card-image">
+				<figure className="image is-1by1">
+					<img src={image} alt="pda logo" />
+				</figure>
+			</div>
+			<div className="card-content">
+				<div className="media-content">
+					<p className="title is-4">{title}</p>
+					<p className="subtitle is-6">{handle}</p>
+				</div>
 			</div>
 		</div>
 	);
