9ec060c6d75299bc0064decea31cc319a2a9c175
chandrasekharanil <chandrasekharanil104@gmail.com>
2022-12-04 23:41:00 -0600

    Second Commit for Props

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,16 +2,16 @@ import React from "react";
 
 // Creating a Separate file for Profile Component.
 
-const ProfileCard = (props) => {
+const ProfileCard = ({ title, handle }) => {
+	console.log("TITLE & HANDLE", title, handle);
 	{
 		/* Props are consumed here which are passed down from App.js*/
 	}
-	// console.log("PROPS ARE: ", props);    // Here you can see the props which are passed from App.js. Props is an object with the attributes specified on the component.
+	// console.log("PROPS ARE: ", props);    // Here you can see the props which are passed from App.js. Props is an object with the attributes specified on the component. NOTE: this can only be seen if props are specified as function arguments. Not when they are destructured.
 	return (
 		<div>
 			<div>
-				The title is {props.title} and the social media handle is {props.handle}
-				.{" "}
+				The title is {title} and the social media handle is {handle}.
 			</div>
 		</div>
 	);
