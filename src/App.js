4e9be46d2fbadab275800a9a6a537c586ca595d3
chandrasekharanil <chandrasekharanil104@gmail.com>
2022-12-04 23:40:16 -0600

    Add and learn to use Props to communicate from parent to child

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ const App = function () {
 	return (
 		<div>
 			<div>Personal Digital Assitant </div>
-			<ProfileCard /> {/* Reusability of the component */}
-			<ProfileCard />
-			<ProfileCard />
+			{/* Reusability of the component */}
+			{/* Below the attributes are declared for the components which are called props. Props are passed from parent to child as an object. I.E from APP.js to ProfileCard.js */}
+			<ProfileCard title="siri" handle="@Siri10" />{" "}
+			<ProfileCard title="cortana" handle="Cortana07" />
+			<ProfileCard title="alexa" handle="Alexa23" />
 		</div>
 	);
 };
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,8 +2,19 @@ import React from "react";
 
 // Creating a Separate file for Profile Component.
 
-const ProfileCard = () => {
-	return <div>Profile Card Component!!!</div>;
+const ProfileCard = (props) => {
+	{
+		/* Props are consumed here which are passed down from App.js*/
+	}
+	// console.log("PROPS ARE: ", props);    // Here you can see the props which are passed from App.js. Props is an object with the attributes specified on the component.
+	return (
+		<div>
+			<div>
+				The title is {props.title} and the social media handle is {props.handle}
+				.{" "}
+			</div>
+		</div>
+	);
 };
 
 export default ProfileCard; // exporting the component.
