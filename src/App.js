f89c4d61788485195955de1d61d3b9d00e0bbbbb
chandrasekharanil <chandrasekharanil104@gmail.com>
2022-12-05 23:24:57 -0600

    Final Commit for Animals Project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import "bulma/css/bulma.css";
+import "./App.css";
 import React, { useState } from "react";
+import AnimalsShow from "./AnimalsShow";
 
-const animalsArray = ["wolf", "fox", "gator", "lion", "eagle", "tiger"];
+const animalsArray = ["dog", "bird", "gator", "cow", "cat", "horse"];
 const animalsFunction = () => {
-	return animalsArray[Math.floor(Math.random() * animalsArray.length)];
+	return animalsArray[Math.floor(Math.random() * animalsArray.length)]; // creating a random animals array from the given animalsArray.
 };
 
 const App = function () {
 	const [animals, setAnimals] = useState([]);
+
+	const renderedAnimals = animals.map((animal, index) => {
+		return <AnimalsShow type={animal} key={index} />; // taking the animals state and adding map function so that it iterates over that array and creates a new AnimalsShow component with type of animal from the map function.
+	});
 
 	const handleClick = () => {
 		setAnimals([...animals, animalsFunction()]); // spreads the existing array and then adds new element to that array. NOTE: Very important to remember whenever we are using state with array!!!!!!
 	};
 
 	return (
-		<div>
+		<div className="app">
 			<button onClick={handleClick}>Add Animal</button>
-			<div>{animals}</div>
+			<div className="animal-list">{renderedAnimals}</div>{" "}
+			{/* Rendering the animals here*/}
 		</div>
 	);
 };
